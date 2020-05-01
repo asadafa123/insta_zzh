5164307cb061584394dfba6bb10d293e1e23b710
asadafa123 <519791790@qq.com>
2020-05-01 01:57:49 -0400

    Display static page

--- Insta/urls.py
+++ Insta/urls.py
@@ -15,8 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-
+from Insta.views import helloworld
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('Insta/', include('Insta.urls'))
+    path('', helloworld.as_view(), name='helloworld')
 ]
